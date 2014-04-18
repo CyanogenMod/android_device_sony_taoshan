5225d4efefaba3d8c2ab48f79664fe060a63917d
Varun Chitre <varun.chitre15@gmail.com>
2014-04-18 13:39:02 +0530

    Initial support for TWRP and misc cleanup
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,8 +98,7 @@ TARGET_RECOVERY_FSTAB = device/sony/taoshan/rootdir/root/fstab.qcom
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/taoshan/custombootimg.mk
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/taoshan/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/sony/taoshan/recovery/graphics.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/sony/taoshan/recovery/graphics/graphics.c
 
 TARGET_NO_RPC := true
 TARGET_PROVIDES_GPS_LOC_API := true
@@ -109,6 +108,27 @@ BOARD_HAVE_NEW_QC_GPS := true
 BOARD_USES_QC_TIME_SERVICES := true
 
 TARGET_POWERHAL_NO_TOUCH_BOOST := true
+
+# TWRP configs
+DEVICE_RESOLUTION := 480x854
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/taoshan/recovery/graphics/twrpgraphics.c
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
 
 BOARD_SEPOLICY_DIRS += \
     device/sony/taoshan/sepolicy
