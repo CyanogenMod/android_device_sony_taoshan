72c4ac6070e56440ca0a11312cb69f31ba7cf3c9
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-07-23 17:12:39 -0400

    taoshan: cleanup mess
    Change-Id: Id3505d554a922a16f84dd782808566f64ebe576a
    Signed-off-by: Arnav Gosain <nolinuxnoparty@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,64 +24,71 @@ include device/sony/qcom-common/BoardConfigCommon.mk
 
 USE_CAMERA_STUB := false
 
+TARGET_SPECIFIC_HEADER_PATH += device/sony/taoshan/include
+
+# Cflags
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+COMMON_GLOBAL_CFLAGS += -DSONY_CAM_PARAMS
+
+# Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_CPU_VARIANT := krait
 BOARD_VENDOR_PLATFORM := taoshan
 TARGET_BOOTLOADER_BOARD_NAME := qcom
 
+# OTA Assert
 TARGET_OTA_ASSERT_DEVICE := C2105,C2104,c2105,c2104,taoshan
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-
+# Kernel
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 # the androidboot.hardware has impact on loading .rc files
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+TARGET_KERNEL_SOURCE := kernel/sony/msm8930
+TARGET_KERNEL_CONFIG := cyanogenmod_taoshan_defconfig
 
+# Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x108BB9E
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1711276032
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 
-BOARD_EGL_CFG := device/sony/taoshan/rootdir/system/lib/egl/egl.cfg
-
+# Vold
 BOARD_VOLD_MAX_PARTITIONS := 33
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
-TARGET_KERNEL_SOURCE := kernel/sony/msm8930
-TARGET_KERNEL_CONFIG := cyanogenmod_taoshan_defconfig
-
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+# Graphics
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_QCOM_AUDIO_VARIANT := caf
 TARGET_QCOM_DISPLAY_VARIANT := caf
 TARGET_QCOM_MEDIA_VARIANT := caf
 TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+BOARD_EGL_CFG := device/sony/taoshan/rootdir/system/lib/egl/egl.cfg
 
 # GPS
 BOARD_HAVE_NEW_QC_GPS := true
 
-TARGET_SPECIFIC_HEADER_PATH += device/sony/taoshan/include
-
+# Audio
+TARGET_QCOM_AUDIO_VARIANT := caf
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 
+# QC AV Enhancements
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
+# Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-COMMON_GLOBAL_CFLAGS += -DSONY_CAM_PARAMS
 
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
@@ -101,6 +108,7 @@ WIFI_DRIVER_MODULE_NAME          := "wlan"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 
+# Bluetooth
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/taoshan/bluetooth
@@ -111,10 +119,13 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/taoshan/custombootimg.mk
 
+# QC Time Services
 BOARD_USES_QC_TIME_SERVICES := true
 
+# PowerHAL
 TARGET_POWERHAL_NO_TOUCH_BOOST := true
 
+# Hardware Class
 BOARD_HARDWARE_CLASS := device/sony/taoshan/cmhw
 
 # TWRP configs
