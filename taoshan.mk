652844ae86161a73f4c18a22bc447b51ce80a3e6
varunchitre15 <varun.chitre15@gmail.com>
2014-07-29 18:47:09 +0400

    taoshan: let wcnss use the wifi config
    WCNSS requires the configuration file to be present in /system/etc/wifi. Without it the service will fail
    
    Change-Id: Ib11cf7fb6c779454eda713d042341b1c8bd776a7

--- taoshan.mk
+++ taoshan.mk
@@ -97,6 +97,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/rootdir/system/etc/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/rootdir/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 # Two stage boot
