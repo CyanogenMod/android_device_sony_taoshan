27eb0ef816997372bd1594d65ea078ebbec7bbce
Chirayu Desai <cdesai@cyanogenmod.org>
2014-11-02 11:02:29 +0530

    Inherit from msm8960-common
    * msm8960-common then inherits from qcom-common.
    * This doesn't change anything currently, just adds one more
      common tree in between - however, later on, we can start moving
      things to this new tree as needed, from both taoshan
      and qcom-common
    
    Change-Id: Ib0a5bca4569501e8504ae18b04c98f59130fe4be

--- taoshan.mk
+++ taoshan.mk
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/sony/taoshan/taoshan-vendor.mk)
 $(call inherit-product, device/sony/common/resources.mk)
-$(call inherit-product, device/sony/qcom-common/qcom-common.mk)
+$(call inherit-product, device/sony/msm8960-common/msm8960.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
