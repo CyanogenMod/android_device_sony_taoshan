ad20ccaf1a5952485668f812e442ff3bc0d93d68
Varun Chitre <varun.chitre15@gmail.com>
2014-11-11 18:02:20 +0100

    taoshan: move bootrec to a better place and remove unneeded header
    Change-Id: I52512739f3b7e92382b38fb0ec37a02913d54978

--- custombootimg.mk
+++ custombootimg.mk
@@ -5,7 +5,7 @@ $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
 INITSH := device/sony/taoshan/combinedroot/init.sh
-BOOTREC_DEVICE := $(PRODUCT_OUT)/recovery/bootrec-device
+BOOTREC_DEVICE := $(LOCAL_PATH)/recovery/root/etc/bootrec-device
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(INITSH) $(BOOTREC_DEVICE) $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/extract_elf_ramdisk $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
