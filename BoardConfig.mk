25058c1bc49a238f9f1cfda82c7422ed067a9501
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-08 18:17:34 +0100

    improve inheritance hierachy, clean-up & consolidate msm8960-common
    Change-Id: I3b189d0237794082371f4d25cd221c37bb7825f3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,27 +16,15 @@
 
 include vendor/sony/taoshan/BoardConfigVendor.mk
 
-# inherit qcom common sepolicies
-include device/qcom/sepolicy/sepolicy.mk
-
-# inherit from Sony common
-include device/sony/common/BoardConfigCommon.mk
-
 # inherit from msm8960-common
 include device/sony/msm8960-common/BoardConfigCommon.mk
 
 USE_CAMERA_STUB := false
 
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_CPU_VARIANT := krait
 BOARD_VENDOR_PLATFORM := taoshan
 TARGET_BOOTLOADER_BOARD_NAME := qcom
 
 TARGET_OTA_ASSERT_DEVICE := C2105,C2104,c2105,c2104,taoshan
-
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
@@ -65,23 +53,10 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/
 TARGET_KERNEL_SOURCE := kernel/sony/msm8930
 TARGET_KERNEL_CONFIG := cyanogenmod_taoshan_defconfig
 
-BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_QCOM_BSP := true
-
-# Bionic
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-
-# Enable Minikin text layout engine (will be the default soon)
-USE_MINIKIN := true
-
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
 
 # MMap compatibility
 BOARD_USES_LEGACY_MMAP := true
-
-# Use dlmalloc
-MALLOC_IMPL := dlmalloc
 
 # GPS
 BOARD_HAVE_NEW_QC_GPS := true
@@ -90,10 +65,9 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/taoshan/include
 
 BOARD_RIL_NO_CELLINFOLIST := true
 
-BOARD_USES_ALSA_AUDIO := true
+# Audio
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-QCOM_ANC_HEADSET_ENABLED := false
 AUDIO_FEATURE_ENABLED_FM := true
 
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
@@ -120,8 +94,7 @@ WIFI_DRIVER_MODULE_NAME          := "wlan"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/taoshan/bluetooth
 
 BOARD_RIL_NO_SEEK := true
@@ -138,27 +111,10 @@ TARGET_POWERHAL_NO_TOUCH_BOOST := true
 
 BOARD_HARDWARE_CLASS := device/sony/taoshan/cmhw
 
-# TWRP configs
-DEVICE_RESOLUTION := 480x854
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_FLASH_FROM_STORAGE := true
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_INCLUDE_JB_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
-TW_INCLUDE_FUSE_EXFAT := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 255
-TW_NO_USB_STORAGE := true
-
 TARGET_USES_LOGD := false
+
+# inherit qcom common sepolicies
+include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/sony/taoshan/sepolicy
