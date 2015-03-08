24520faf3397b5eb9f47e7b928901bfbede5a762
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-08 18:17:34 +0100

    improve inheritance hierachy, consolidate msm8960-common
    Change-Id: I3b189d0237794082371f4d25cd221c37bb7825f3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,24 +19,15 @@ include vendor/sony/taoshan/BoardConfigVendor.mk
 # inherit qcom common sepolicies
 include device/qcom/sepolicy/sepolicy.mk
 
-# inherit from Sony common
-include device/sony/common/BoardConfigCommon.mk
-
 # inherit from msm8960-common
 include device/sony/msm8960-common/BoardConfigCommon.mk
 
 USE_CAMERA_STUB := false
 
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_CPU_VARIANT := krait
 BOARD_VENDOR_PLATFORM := taoshan
 TARGET_BOOTLOADER_BOARD_NAME := qcom
 
 TARGET_OTA_ASSERT_DEVICE := C2105,C2104,c2105,c2104,taoshan
-
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
@@ -68,9 +59,6 @@ TARGET_KERNEL_CONFIG := cyanogenmod_taoshan_defconfig
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_QCOM_BSP := true
 
-# Bionic
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
@@ -79,9 +67,6 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # MMap compatibility
 BOARD_USES_LEGACY_MMAP := true
-
-# Use dlmalloc
-MALLOC_IMPL := dlmalloc
 
 # GPS
 BOARD_HAVE_NEW_QC_GPS := true
@@ -93,7 +78,6 @@ BOARD_RIL_NO_CELLINFOLIST := true
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-QCOM_ANC_HEADSET_ENABLED := false
 AUDIO_FEATURE_ENABLED_FM := true
 
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
