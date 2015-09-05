511a0fdfd51283cc8244e7689692ace5ceb86696
Olivier K <olivier@olivierk.eu>
2015-09-05 15:18:01 +0200

    init: Disable LED later
    Otherwise, it'll only be disabled when booting Android.
    
    Change-Id: I0338053d4603456b140ca24a1894d3859b1b1ca6

--- combinedroot/init.sh
+++ combinedroot/init.sh
@@ -42,10 +42,6 @@ load_image=/sbin/ramdisk.cpio
 if [ -s /dev/keycheck ] || busybox grep -q warmboot=0x77665502 /proc/cmdline ; then
 	busybox echo 0 > /sys/module/msm_fb/parameters/align_buffer
 	busybox echo 'RECOVERY BOOT' >>boot.txt
-	# orange led for recoveryboot
-	busybox echo 255 > ${BOOTREC_LED_RED}
-	busybox echo 100 > ${BOOTREC_LED_GREEN}
-	busybox echo 0 > ${BOOTREC_LED_BLUE}
 	busybox echo 100 > ${BOOTREC_VIBRATOR}
 	# recovery ramdisk
 	busybox mknod -m 600 ${BOOTREC_FOTA_NODE}
@@ -56,14 +52,15 @@ if [ -s /dev/keycheck ] || busybox grep -q warmboot=0x77665502 /proc/cmdline ; t
 	load_image=/sbin/ramdisk-recovery.cpio
 else
 	busybox echo 'ANDROID BOOT' >>boot.txt
-	# poweroff LED
-	busybox echo 0 > ${BOOTREC_LED_RED}
-	busybox echo 0 > ${BOOTREC_LED_GREEN}
-	busybox echo 0 > ${BOOTREC_LED_BLUE}
 fi
 
 # kill the keycheck process
 busybox pkill -f "busybox cat ${BOOTREC_EVENT}"
+
+# poweroff LED
+busybox echo 0 > ${BOOTREC_LED_RED}
+busybox echo 0 > ${BOOTREC_LED_GREEN}
+busybox echo 0 > ${BOOTREC_LED_BLUE}
 
 # unpack the ramdisk image
 busybox cpio -i < ${load_image}
