f285ffbd462c1a5c7ad272c5063045854dd30410
Olivier K <olivier@olivierk.eu>
2015-09-19 12:41:08 +0200

    taoshan: Remove vendor init
    * Was causing several issues with recoveries.
    * C2104/C2105 don't differ significantly, so let's call them both "taoshan".
    * Also, re-organize some makefiles
    
    This reverts commit e0bd0d82b51aba93bb8cbbcaf6fd7d950b014837.
    
    Change-Id: I89614054c9b1108c8203c75bd679a35bd84d1433

--- full_taoshan.mk
+++ full_taoshan.mk
@@ -12,20 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/sony/taoshan/full_taoshan.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common Sony resources
+$(call inherit-product, device/sony/common/resources.mk)
 
-# Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=C2105 \
-    BUILD_FINGERPRINT="Sony/C2105/C2105:4.2.2/15.3.A.1.17/Android.1016:user/release-keys" \
-    PRIVATE_BUILD_DESC="C2105-user 4.2.2 JDQ39 Android.1016 test-keys"
+# Inherit device config
+$(call inherit-product, device/sony/taoshan/taoshan.mk)
 
-# Override some inherited defines
+# Device identifier
 PRODUCT_DEVICE := taoshan
-PRODUCT_NAME := cm_taoshan
+PRODUCT_NAME := full_taoshan
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia L
+PRODUCT_MANUFACTURER := Sony
