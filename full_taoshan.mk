43279e489ee7d2f69ef17b20a03d33344c044876
Olivier K <olivier@olivierk.eu>
2015-09-19 12:41:08 +0200

    taoshan: Remove vendor init
    * While the automatic model number detection was nice to have, it causes several issues with recoveries. C2104 and C2105 don't differ significantly, so let's just call them both "Xperia L" and "taoshan".
    
    Change-Id: I89614054c9b1108c8203c75bd679a35bd84d1433

---
+++ full_taoshan.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit common Sony resources
+$(call inherit-product, device/sony/common/resources.mk)
+
+# Inherit device config
+$(call inherit-product, device/sony/taoshan/taoshan.mk)
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=C2105 \
+    BUILD_FINGERPRINT="Sony/C2105/C2105:4.2.2/15.3.A.1.17/Android.1016:user/release-keys" \
+    PRIVATE_BUILD_DESC="C2105-user 4.2.2 JDQ39 Android.1016 test-keys"
+
+# Device identifier
+PRODUCT_DEVICE := taoshan
+PRODUCT_NAME := full_taoshan
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia L
+PRODUCT_MANUFACTURER := Sony
