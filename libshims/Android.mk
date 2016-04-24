852d9be42e4cae9bf86fef5d65ec9bfa92ef0589
AdrianDC <radian.dc@gmail.com>
2016-04-25 03:42:04 +0530

    taoshan: Provide a camera SHIM library for SONY libs
     * Based upon 5.1 frameworks/native sources
     * Updated with the related 6.0 source updates
        as well as deletions to match jellybean sizes
        to prevent breakage and to all compilation warnings
     * Provide additional symbols for compatibility
     * Only SHIM against camera.vendor.qcom.so
    
     * Thanks to :
      - Christopher R. Palmer for the "Properly shim VectorImpl" and
         SensorManager fixes from "Shim the camera hal to get it loading"
      - Otto Juntunen for pointing out the Looper.cpp requirement
    
    Change-Id: Idd03ff4d7226108024e734c002102b47d7dbeace
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    Signed-off-by: Sohamlad7 <sohamlad7@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,14 +1,69 @@
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 
-# libqc-opt
+# libshim_camera
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    gui/SensorManager.cpp \
+    ui/GraphicBuffer.cpp \
+    ui/GraphicBufferAllocator.cpp \
+    ui/GraphicBufferMapper.cpp \
+    utils/Looper.cpp \
+    utils/VectorImpl.cpp \
+    symbols/Parcel.cpp
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include \
+    external/safe-iop/include
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libhardware \
+    libui \
+    libgui \
+    libbinder \
+    libutils \
+    libsync
+
+LOCAL_MODULE := libshim_camera
+ 
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+# libshim_qcopt
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-     icu53.c
+    symbols/icu53.c
 
-LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
+LOCAL_SHARED_LIBRARIES := \
+    libicuuc \
+    libicui18n
+
+LOCAL_CFLAGS += \
+    -Wall
+
 LOCAL_MODULE := libshim_qcopt
+
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
