8dc4b320c1178c9186b6bab239c5865021e5ca0c
AdrianDC <radian.dc@gmail.com>
2016-06-22 02:06:08 +0200

    taoshan: Update custombootimg for renamed toybox_init
     * Toybox is moved to its own path name to avoid conflicts
        between the init ramdisk toybox and the one used from root
        inside Android and CyanogenRecovery
    
    Change-Id: I5358758cc151eb88bceb49fe8a2f0afdcce32fcd

--- custombootimg.mk
+++ custombootimg.mk
@@ -17,7 +17,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(r
 	$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/sbin/
-	$(hide) cp $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/combinedroot/sbin/
+	$(hide) cp $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/combinedroot/sbin/toybox_init
 
 	$(hide) cp $(INITSONY) $(PRODUCT_OUT)/combinedroot/sbin/init_sony
 	$(hide) chmod 755 $(PRODUCT_OUT)/combinedroot/sbin/init_sony
