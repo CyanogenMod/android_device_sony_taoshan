a4f0b857f035251d3d1a2442ecc225b25bd6f697
Adrian DC <radian.dc@gmail.com>
2016-07-03 11:40:09 +0200

    taoshan: liblights: Add TARGET_PROVIDES_LIBLIGHT dependency
    Change-Id: I58691605b979c35828551b593b915351a98ae740

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -17,3 +19,5 @@ LOCAL_MODULE := lights.msm8960
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
