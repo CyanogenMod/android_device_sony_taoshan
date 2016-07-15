bf1f39110d894f2b651b8d7d8c300d71b947fd2d
Adrian DC <radian.dc@gmail.com>
2016-07-15 16:13:16 +0200

    taoshan: Remove now obsolete shim library
    * Adapted to the new tree structure
    
    This reverts commit 71417c3e3bc8ce79bd6715397ca402884aba6408.
    
    Change-Id: I81c24ce883bbf98b0e6545f369c7f047d20d1e14

--- product/camera.mk
+++ product/camera.mk
@@ -3,10 +3,6 @@ PRODUCT_PACKAGES += \
     camera.qcom \
     libstlport
 
-# Camera SHIM packages
-PRODUCT_PACKAGES += \
-    libshim_qcopt
-
 # Camera properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
