43e4d4b31b77482ebb7a6c02214d78c3deeede83
Adrian DC <radian.dc@gmail.com>
2016-07-21 23:23:26 +0200

    taoshan: release: Unified UserData migration path
     * The partition /dev/block/platform/msm_sdcc.1/by-name/SDCard
        existence is tested upon ROM flash start.
    
       - If found, the whole Unify Userdata procedure will start
       - If not found, the data partition will be test mounted,
          if unmountable, an assert about a broken data partition
          and request for a Factory Reset will be displayed
    
     * All informations & requirements of the unification
        will be shown to the users through related warnings & texts
    
     * When data can be lost, the process aborts & asks the user
        to save his data, if he wants to ignore the losses or risks,
        a second flash will go forward & accept the previous warning
    
     * The restore_sdcard sources are added to the project
        in order to pack a zip and link it from the CM Wiki.
        It uses the same logic as the unified installer to
        restore the original physical SDCard partitions layout
    
     * The automated reboots to perform the resizing and formats
        are used to allow the mmc partitions to be redetected
        upon a reboot, otherwise the old table remains used,
        therefore the flashing cycle allows to perform
        an user-interaction-less full reliable procedure
    
     * An arbitrary ASCII number for Y and N is used to confirm
        the unification of UserData in order to ensure the script
        has been run & that the verifications were performed.
        As usual with run_program, the value is returned * 0xFF
    
     * The releasetools.py path files copy implementation was inspired
        by the similar use in android_device_samsung_aries-common
    
     * An additional safety was added on final tests to block
        users of old outdated TWRP recovery from doing anything,
        update to the latest official or XDA release is needed.
        The main reason for this, apart the unification changes,
        is that an unofficial 12.1 with TWRP introduced
        an fstab error and may brick the phone if a user
        wipes / formats internal storage, therefore block
        and make sure it is used as little as possible
    
     * As much safeties as possible are introduced to avoid
        the user from randomly flashing the ROM or the zip,
        and if he did change filesystems or partitions names
        on purpose to unsupported values, explain and abort
    
    Change-Id: I8af554eb0183405fcac7cdf8e2e324ecdc6db8ae
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ releasetools/unify_userdata/main.sh
@@ -0,0 +1,361 @@
+#!/sbin/sh
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2016 Adrian DC
+#
+# Core script of the UserData Unify Tools
+#
+
+#------------------------------------------------------------
+# Execution variables
+dir_bin=/tmp/install/bin;
+dir_script=/tmp/install/unify_userdata;
+exec_failed=78;
+exec_success=89;
+source ${dir_script}/variables.sh;
+if [ -z "${variables_set}" ]; then
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# Shared helper tools
+source ${dir_script}/edify_tools.sh;
+source ${dir_script}/part_tools.sh;
+source ${dir_script}/prop_tools.sh;
+
+#------------------------------------------------------------
+# Abort if a partition change was performed before
+if [ -e ${flag_partitions_changed} ]; then
+  ui_print ' ';
+  ui_print 'You already performed a partition change';
+  ui_print 'Reboot to recovery before continuing...';
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# Handle the usage of an old dangerous TWRP version
+if [ -e /twres/twrp ] || [ -e /res/twrp ]; then
+  if [ $(prop_default_date_timestamp) -lt ${twrp_timestamp_safe} ]; then
+    ui_print ' ';
+    ui_print 'The TWRP version you are using is outdated,';
+    ui_print 'and may introduce a lot of problems...';
+    ui_print 'Please update to latest TWRP Recovery';
+    exit ${exec_failed};
+  fi;
+fi;
+
+#------------------------------------------------------------
+# If UserData resizing was programmed by the user
+if [ -e ${cache_flag_resize_userdata} ]; then
+  ${toybox} rm -f ${cache_flag_resize_userdata};
+
+  # Extract the resize_userdata.zip and reboot on it
+  ui_print ' ';
+  ui_print 'Prepare the UserData resizing...';
+  ${toybox} cp -f ${tmp_resize_userdata_zip} ${cache_resize_userdata_zip};
+  ${toybox} echo '--update_package='${cache_resize_userdata_zip} > ${cache_recovery_command};
+
+  # If the preparation of the recovery command failed
+  if [ ! -e ${cache_recovery_command} ] || \
+     [ ! -e ${cache_resize_userdata_zip} ]; then
+    ui_print ' ';
+    ui_print 'Failed starting a recovery script';
+    ui_print 'Search for help on the ROM forums...';
+    ${toybox} rm -f ${cache_recovery_command};
+    ${toybox} rm -f ${cache_resize_userdata_zip};
+    exit ${exec_failed};
+  fi;
+
+  # End of the modifications
+  ui_print 'Rebooting to recovery automatically';
+  ui_print ' ';
+
+  # Reboot to Recovery
+  /sbin/sleep 10;
+  /sbin/reboot recovery;
+
+  # Return a failure if we came here
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# If the SDCard partition does not exists, ROM flashable
+if [ ! -e ${parts_by_name}/${part_sdcard_name} ]; then
+
+  # If the unification process left a resize_userdata zip in cache
+  if [ -e "${cache_resize_userdata_zip}" ]; then
+    rm -f ${cache_resize_userdata_zip};
+  fi;
+
+  # If the UserData partition is not mountable, abort
+  part_unmount '/data';
+  part_unmount '/sdcard';
+  if ! part_mount '/data' ${mmc_block}p${part_data_num} 'ext4 f2fs'; then
+    ui_print ' ';
+    ui_print 'UserData could not be mounted (ext4/f2fs ?)';
+    ui_print 'Please do a Factory Reset before continuing';
+    exit ${exec_failed};
+
+  # If the UserData is mountable, check the storage.xml file
+  elif ${toybox} grep -q 'primaryStorageUuid="primary_physical"' ${data_storage_xml}; then
+    ui_print ' ';
+    ui_print 'Finalize the migration, reset storages';
+    ui_print 'Deleting '${data_storage_xml};
+    ui_print ' ';
+    ${toybox} rm -f ${data_storage_xml};
+  fi;
+
+  # If everything checks out for a ROM flash, return a success
+  exit ${exec_success};
+fi;
+
+#------------------------------------------------------------
+# Script introduction
+ui_print ' ';
+ui_print '       =========================';
+ui_print '       | UserData Unifier Tool |';
+ui_print '       =========================';
+ui_print ' ';
+
+#------------------------------------------------------------
+# If the flashable zip is stored on a partition going to be formatted
+if [ ! -z "$(${toybox} echo ${flashable_zip} | grep -E '/data/|/sdcard/|/sdcard_legacy/')" ]; then
+  ui_print 'The flashable zip is stored on a partition';
+  ui_print 'that will be lost by the operation...';
+
+  # On first flash, abort & warn about a second flash
+  if [ ! -e ${flag_flashable_internal} ]; then
+    ui_print ' ';
+    ui_print 'You could save the zip on a MicroSD';
+    ui_print 'or use "adb sideload pathtofile.zip"';
+    ui_print ' ';
+    ui_print 'Otherwise, if you really want to use';
+    ui_print 'the current zip, flash it again';
+    ${toybox} echo 1 > ${flag_flashable_internal};
+    exit ${exec_failed};
+  fi;
+
+  # On second flash, allow continuing & losing the zip
+  ui_print 'You are agreeing to use the zip';
+  ui_print ' ';
+fi;
+
+#------------------------------------------------------------
+# Announce the partitions verifications
+ui_print 'Starting the partitions verifications...';
+ui_print ' ';
+
+# Unmount the data related partitions
+part_unmount '/data';
+part_unmount '/sdcard_legacy';
+part_unmount '/sdcard';
+
+#------------------------------------------------------------
+# If identifiers of the Cache partition are missing, abort
+if [ ! -e ${parts_by_name}/${part_cache_name} ] || \
+   [ ! -e ${mmc_block}p${part_cache_num} ] || \
+   [ ! part_exists ${part_cache_name} ${part_cache_num} ]; then
+  ui_print 'Device has issues with Cache partition';
+  ui_print 'Please search for help on forums...';
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# If identifiers of the SDCard partition are missing, abort
+if [ ! -e ${parts_by_name}/${part_sdcard_name} ] || \
+   [ ! -e ${mmc_block}p${part_sdcard_num} ] || \
+   [ ! part_exists ${part_sdcard_name} ${part_sdcard_num} ]; then
+  ui_print 'Device has issues with SDCard partition';
+  ui_print 'Please search for help on forums...';
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# If identifiers of the UserData partition are missing, abort
+if [ ! -e ${parts_by_name}/${part_data_name} ] || \
+   [ ! -e ${mmc_block}p${part_data_num} ] || \
+   [ ! part_exists ${part_data_name} ${part_data_num} ]; then
+  ui_print 'Device has issues with UserData partition';
+  ui_print 'Please search for help on forums...';
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# Mount the Cache partition
+if ! part_mount '/cache' ${mmc_block}p${part_cache_num} 'ext4 f2fs'; then
+  ui_print 'Cache could not be mounted (ext4/f2fs ?)';
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# Try to mount the SDCard partition
+if ! part_mount '/sdcard_legacy' ${mmc_block}p${part_sdcard_num} 'ext4 exfat'; then
+  ui_print 'SDCard could not be mounted (ext4/exfat ?)';
+
+  # On first flash, abort & warn about a second flash
+  if [ ! -e ${flag_wipe_sdcard_attempt} ]; then
+    ui_print 'Please verify your partition file system';
+    ui_print ' ';
+    ui_print 'If you are sure you want to format SDCard, ';
+    ui_print 'and accept the losses, flash this zip again';
+    ${toybox} echo 1 > ${flag_wipe_sdcard_attempt};
+    exit ${exec_failed};
+  fi;
+
+  # On second flash, allow continuing & losing UserData
+  ui_print 'You are agreeing to format your SDCard...';
+
+# If the SDCard partition could be mounted
+else
+  # Check if SDCard is empty
+  if [ ! -d /sdcard_legacy ] || [ -n "$(${toybox} find /sdcard_legacy/ -type f)" ]; then
+    ui_print 'SDCard partition still contains files';
+
+    # On first flash, abort & warn about a second flash
+    if [ ! -e ${flag_wipe_sdcard_attempt} ]; then
+      ui_print 'Please copy your data from internal SDCard';
+      ui_print 'Do not format the partition yourself';
+      ui_print ' ';
+      ui_print 'If you are sure you want to remove SDCard,';
+      ui_print 'and accept the losses, flash this zip again';
+      ${toybox} echo 1 > ${flag_wipe_sdcard_attempt};
+      exit ${exec_failed};
+    fi;
+
+    # On second flash, allow continuing & losing SDCard
+    ui_print 'You are agreeing to remove internal SDCard...';
+    ui_print ' ';
+
+  # If SDCard is empty, continue
+  else
+    ui_print 'SDCard partition is empty';
+  fi;
+  part_unmount '/sdcard_legacy';
+fi;
+
+#------------------------------------------------------------
+# Try to mount the UserData partition
+if ! part_mount '/data' ${mmc_block}p${part_data_num} 'ext4'; then
+  ui_print ' ';
+  ui_print 'UserData could not be mounted (ext4 ?)';
+  ui_print 'Please verify your partition file system';
+  ui_print ' ';
+  ui_print 'UserData needs to be using ext4,';
+  ui_print 'so please perform a Factory Reset';
+  exit ${exec_failed};
+
+# If the UserData partition could be mounted
+else
+  # Check if UserData is empty
+  if [ ! -d /data ] || [ -n "$(${toybox} find /data/ -type f)" ]; then
+    ui_print 'UserData partition still contains files';
+
+    # On first flash, abort & warn about a second flash
+    if [ ! -e ${flag_wipe_data_attempt} ]; then
+      ui_print ' ';
+      ui_print 'Please backup your data to avoid losses';
+      ui_print 'UserData will try to be kept but be safe';
+      ui_print ' ';
+      ui_print 'If you are sure you want to keep UserData,';
+      ui_print 'and accept the risks, flash this zip again';
+      ${toybox} echo 1 > ${flag_wipe_data_attempt};
+      exit ${exec_failed};
+    fi;
+
+    # On second flash, allow continuing & risking UserData
+    ui_print 'You are agreeing to risk your UserData...';
+
+  # If UserData is empty, continue
+  else
+    ui_print 'UserData partition is empty';
+  fi;
+fi;
+
+# Unmount the UserData partition
+part_unmount '/data';
+
+#------------------------------------------------------------
+# Announce the process start
+ui_print ' ';
+ui_print 'Starting the UserData unification...';
+
+#------------------------------------------------------------
+# Read the new UserData partition start and end
+part_data_start=$(part_get_start ${part_data_name});
+part_data_end=$(part_get_end ${part_sdcard_name});
+
+# If the partition start and end are correct
+if [ ! -z "${part_data_start}" ] && \
+   [ ! -z "${part_data_end}" ] && \
+   [ ${part_data_start} -lt ${part_data_end} ]; then
+
+  # Delete partitions and create Userdata
+  ${toybox} echo 1 > ${flag_partitions_changed};
+  part_unmount ${mmc_block}p${part_data_num};
+  part_unmount ${mmc_block}p${part_sdcard_num};
+  part_unify_state='Deleting SDCard' && \
+    ${sgdisk} --delete=${part_sdcard_num} ${mmc_block};
+  test $? -eq 0 && part_unify_state='Deleting UserData' && \
+    ${sgdisk} --delete=${part_data_num} ${mmc_block};
+  test $? -eq 0 && part_unify_state='Creating new UserData' && \
+    ${sgdisk} --new=${part_data_num}:${part_data_start}:${part_data_end} ${mmc_block};
+  test $? -eq 0 && part_unify_state='Renaming new UserData' && \
+    ${sgdisk} --change-name=${part_data_num}:${part_data_name} ${mmc_block};
+
+  # If the partitions modifications were successful
+  if [ $? -eq 0 ]; then
+    ui_print 'Partitions modifications done';
+
+  # If the partitions modifications failed
+  else
+    ui_print 'Failure during the modifications';
+    ui_print 'Error on: '$part_unify_state;
+    ui_print 'Search for help on the ROM forums...';
+    exit ${exec_failed};
+  fi;
+
+# If the partitions dimensions are incorrect
+else
+  ui_print 'Failure during the dimensioning';
+  ui_print 'Search for help on the ROM forums...';
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# End of the procedure
+ui_print 'UserData unified successfully';
+ui_print ' ';
+
+#------------------------------------------------------------
+# Initialize a recovery command script
+${toybox} mkdir -p ${cache_recovery};
+${toybox} echo '' > ${cache_recovery_command};
+${toybox} cp -f ${tmp_resize_userdata_zip} ${cache_resize_userdata_zip};
+${toybox} rm -f ${cache_flag_format_userdata_sdcard};
+
+# If the preparation of the recovery command failed
+if [ ! -e ${cache_recovery_command} ] || \
+   [ ! -e ${cache_resize_userdata_zip} ]; then
+  ui_print 'Failed starting a recovery script';
+  ui_print 'Search for help on the ROM forums...';
+  ${toybox} rm -f ${cache_recovery_command};
+  ${toybox} rm -f ${cache_resize_userdata_zip};
+  exit ${exec_failed};
+fi;
+
+#------------------------------------------------------------
+# Automatic rebooting instructions
+ui_print 'Rebooting to recovery automatically,';
+ui_print 'then resizing UserData partition...';
+ui_print ' ';
+
+# Reboot to recovery and resize the UserData
+${toybox} echo '--update_package='${cache_resize_userdata_zip} > ${cache_recovery_command};
+
+#------------------------------------------------------------
+# Reboot to Recovery
+/sbin/sleep 10;
+/sbin/reboot recovery;
+
+# Return a failure if we came here
+exit ${exec_failed};
---
+++ releasetools/unify_userdata/part_tools.sh
@@ -0,0 +1,91 @@
+#!/sbin/sh
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2016 Adrian DC
+#
+# Partitions shared tools
+#
+
+# Function part_mount(point,path,type)
+part_mount()
+{
+  # Partition already mounted
+  if ${toybox} grep -q "${1} " /proc/mounts; then
+    return 0;
+  fi;
+
+  # Prepare the partition mounts
+  ${toybox} mkdir -p ${1};
+  if ${toybox} grep -q "${2} " /proc/mounts; then
+    ${toybox} umount -fl "${2}";
+  fi;
+
+  # Try mounting the partition with each of the file systems
+  for filesystem in ${3}; do
+    if ${toybox} mount -t $filesystem ${2} ${1}; then
+      return 0;
+    fi;
+  done;
+
+  # If the mounts failed, return an error
+  ${toybox} echo "part_mount: Failed on ${1}";
+  return 1;
+}
+
+# Function part_unmount(point)
+part_unmount()
+{
+  # Mount the partition if required
+  if ${toybox} grep -q "${1} " /proc/mounts; then
+    if ! ${toybox} umount -fl "${1}"; then
+      ${toybox} echo "part_umount: Failed on ${1}";
+      return 1;
+    fi;
+  fi;
+
+  return 0;
+}
+
+# Function part_exists(name,num)
+part_exists()
+{
+  # Verify if a partition exists and matches a number
+  if [ ! -z "$(${sgdisk} --print ${mmc_block} | ${toybox} grep ${1}) | ${toybox} grep ' ${2}) ')" ]; then
+    return 1;
+  fi;
+
+  return 0;
+}
+
+# Function part_get_start(name)
+part_get_start()
+{
+  # Return the partition start value
+  ${toybox} echo $(${sgdisk} --print ${mmc_block} \
+                 | ${toybox} grep "${1}" \
+                 | ${toybox} tr -s ' ' \
+                 | ${toybox} cut -d ' ' -f 3);
+}
+
+# Function part_get_end(name)
+part_get_end()
+{
+  # Return the partition start value
+  ${toybox} echo $(${sgdisk} --print ${mmc_block} \
+                 | ${toybox} grep ${1} \
+                 | ${toybox} tr -s ' ' \
+                 | ${toybox} cut -d ' ' -f 4);
+}
+
+# Function part_get_resize2fs(num, length_sub)
+part_get_resize2fs()
+{
+  # Return the partition size for resize2fs
+  partition_size=$(${toybox} cat /proc/partitions \
+                 | ${toybox} grep "p${1}" \
+                 | ${toybox} tr -s ' ' \
+                 | ${toybox} sed 's/\..*//' \
+                 | ${toybox} cut -d ' ' -f 4);
+  partition_block_size=$(($(${toybox} blockdev --getbsz ${mmc_block}p${1}) / 1024));
+  ${toybox} echo $(( (${partition_size} - ${2} / 1024) / ${partition_block_size} ));
+}
