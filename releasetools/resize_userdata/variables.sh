cc8873e06e3a178c3fba5416f49cb957981dfa6a
Adrian DC <radian.dc@gmail.com>
2016-07-21 23:23:26 +0200

    taoshan: release: Unified UserData migration path
     * The partition /dev/block/platform/msm_sdcc.1/by-name/SDCard
        existence is tested upon ROM flash start.
    
       - If found, the whole Unify Userdata procedure will start
       - If not found, the data partition will be test mounted,
          if unmountable, an assert about a broken data partition
          and request for a Factory Reset will be displayed
    
     * All informations & requirements of the unification
        will be shown to the users through related warnings & texts
    
     * When data can be lost, the process aborts & asks the user
        to save his data, if he wants to ignore the losses or risks,
        a second flash will go forward & accept the previous warning
    
     * The restore_sdcard sources are added to the project
        in order to pack a zip and link it from the CM Wiki.
        It uses the same logic as the unified installer to
        restore the original physical SDCard partitions layout
    
     * The automated reboots to perform the resizing and formats
        are used to allow the mmc partitions to be redetected
        upon a reboot, otherwise the old table remains used,
        therefore the flashing cycle allows to perform
        an user-interaction-less full reliable procedure
    
     * An arbitrary ASCII number for Y and N is used to confirm
        the unification of UserData in order to ensure the script
        has been run & that the verifications were performed.
        As usual with run_program, the value is returned * 0xFF
    
     * The releasetools.py path files copy implementation was inspired
        by the similar use in android_device_samsung_aries-common
    
     * An additional safety was added on final tests to block
        users of old outdated TWRP recovery from doing anything,
        update to the latest official or XDA release is needed.
        The main reason for this, apart the unification changes,
        is that an unofficial 12.1 with TWRP introduced
        an fstab error and may brick the phone if a user
        wipes / formats internal storage, therefore block
        and make sure it is used as little as possible
    
     * As much safeties as possible are introduced to avoid
        the user from randomly flashing the ROM or the zip,
        and if he did change filesystems or partitions names
        on purpose to unsupported values, explain and abort
    
    taoshan: release: Detect MultiROM flashing in the unification checks
    
     * When flashing as a MultiROM, the unification path should not be executed,
        even on a device that was unified before, the checks for the physical flag
        that performs an umount / mount of the real data partition breaks
        the install process and the partitions remapping of MultiROM
    
     * Avoid this by detecting the /tmp/mrom_fakebootpart flag
        that will only exist during a MultiROM instalation
    
     * Thanks to nkk71 for going through the sources with me
        to figure out the origin of this issue upon installations
    
    Change-Id: I8af554eb0183405fcac7cdf8e2e324ecdc6db8ae
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ releasetools/resize_userdata/variables.sh
@@ -0,0 +1,34 @@
+#!/sbin/sh
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2016 Adrian DC
+#
+# Shared variables
+#
+
+# Executables paths
+e2fsck=${dir_bin}/e2fsck;
+resize2fs=${dir_bin}/resize2fs_static;
+toybox=${dir_bin}/toybox;
+
+# Recovery paths
+cache_recovery=/cache/recovery;
+cache_recovery_command=${cache_recovery}/command;
+
+# Identifier tags
+flag_partitions_changed=/tmp/flag_partitions_changed;
+
+# UserData paths
+data_storage_xml=/data/system/storage.xml;
+
+# Partitions path constants
+mmc_block=/dev/block/mmcblk0;
+
+# Partitions items constants
+part_data_num=31;
+
+# Partitions dimension constants
+part_data_length_sub=16384;
+
+# Variables are defined
+variables_set='true';
