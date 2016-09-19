d98f2ef2f604b4434bbecf48de722115ebfdedc9
corphish <d97.avinaba@gmail.com>
2016-09-19 20:57:54 +0530

    taoshan: Switch to permissive SELinux
    Change-Id: Ibe674228ff72956c9c4bb8528ff0b570d263c1c5
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # Kernel informations
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Kernel properties
