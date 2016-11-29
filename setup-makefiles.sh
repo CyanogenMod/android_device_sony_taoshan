b4a18f20173d11162507199c21c07d1a98bcf4cd
Olivier Karasangabo <olivier@cyanogenmod.org>
2016-11-29 19:27:51 +0100

    taoshan: Update blob setup
    * Remove unneeded and deprecated blobs
    * Sort the list alphabetically
    
    Change-Id: I70f84f78c64a1821871fa33f7b1c929761e63b17

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -60,24 +60,6 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include \$(BUILD_PREBUILT)
 
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libimage-jpeg-enc-omx-comp
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/lib/libimage-jpeg-enc-omx-comp.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libmmstillomx
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/lib/libmmstillomx.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include \$(BUILD_PREBUILT)
-
 endif
 EOF
 fi
